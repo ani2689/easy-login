d2fe0159fe119848e4637bd50ec95e0d65f43921
ani2689 <ojno7777@gmail.com>
2023-06-26 21:02:16 +0900

    :tada: v2 시작

---
+++ src/main/kotlin/com/ani/easylogin/EasyLoginApplication.kt
@@ -0,0 +1,11 @@
+package com.ani.easylogin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EasyLoginApplication
+
+fun main(args: Array<String>) {
+	runApplication<EasyLoginApplication>(*args)
+}
---
+++ src/test/kotlin/com/ani/easylogin/EasyLoginApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ani.easylogin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EasyLoginApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
