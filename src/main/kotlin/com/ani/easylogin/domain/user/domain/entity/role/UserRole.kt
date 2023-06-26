abf59eb113914aaacdae65c273afc559246665b3
ani2689 <ojno7777@gmail.com>
2023-06-26 20:49:08 +0900

    초기화

--- src/main/kotlin/com/ani/easylogin/domain/user/domain/entity/role/UserRole.kt
+++
@@ -1,6 +0,0 @@
-package com.ani.easylogin.domain.user.domain.entity.role
-
-enum class UserRole (val description: String){
-    BASIC("기본 권한"),
-    ADMIN("관리자 권한")
-}
--- src/main/kotlin/com/ani/easylogin/domain/user/domain/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.ani.easylogin.domain.user.domain.repository
-
-import com.ani.easylogin.domain.user.domain.entity.User
-import org.springframework.data.repository.CrudRepository
-
-interface UserRepository : CrudRepository<User, String> {
-
-}
